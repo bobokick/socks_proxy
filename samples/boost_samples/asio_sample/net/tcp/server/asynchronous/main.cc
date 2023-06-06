3386c71f578d31fa24b97b7dc72f6aca373c0eab
bobokick <1789630364@qq.com>
2023-06-06 10:28:21 +0800

    🧪 test: tcp server updated

--- samples/boost_samples/asio_sample/net/tcp/server/asynchronous/main.cc
+++ samples/boost_samples/asio_sample/net/tcp/server/asynchronous/main.cc
@@ -16,8 +16,11 @@ class TcpConnection
     std::string message_;
     TcpConnection(boost::asio::io_context& io_context):
         socket_(io_context) {}
-    void handleWrite(const boost::system::error_code&, size_t) {}
+    // 关于客户端连接的其他操作将由该函数负责
+    // 如错误，长度信息输出等
+    void handleWrite(const boost::system::error_code& /*error*/, size_t /*bytes_transferred*/) {}
 public:
+    // 用共享智能指针是为了保持TcpConnection对象的存活。
     typedef std::shared_ptr<TcpConnection> CPointer;
     inline static CPointer create(boost::asio::io_context& io_context)
     {
@@ -26,7 +29,10 @@ public:
     tcp::socket& getSocket() { return socket_; }
     void start()
     {
+        // 储存将要发送的数据
         message_ = makeDaytimeString();
+        // 发送服务到客户端
+        // 注意用的是boost::asio::async_write而不是ip::tcp::socket::async_write_some()，是为了保证整个数据块都能被完整发送。
         boost::asio::async_write(socket_, boost::asio::buffer(message_), std::bind(&TcpConnection::handleWrite, this, std::placeholders::_1, std::placeholders::_2));
     }
 };
@@ -35,6 +41,8 @@ class TcpServer
 {
     boost::asio::io_context& io_context_;
     tcp::acceptor acceptor_;
+    // 当startAccept()函数中的异步接收操作完成时，该函数会被调用
+    // 它服务客户端请求时，然后会继续调用startAccept进行下一个接收操作。
     void handleAccept(TcpConnection::CPointer new_connection, const boost::system::error_code& error)
     {
         if (!error)
@@ -43,18 +51,18 @@ class TcpServer
         }
         startAccept();
     }
+    // 创建一个套接字并初始化一个异步接收操作用于等待新连接
     void startAccept()
     {
         TcpConnection::CPointer new_connection = TcpConnection::create(io_context_);
         acceptor_.async_accept(new_connection->getSocket(), std::bind(&TcpServer::handleAccept, this, new_connection, std::placeholders::_2));
     }
 public:
-    // 初始化
+    // 初始化监听对象来监听tcp ipv4上13号端口的连接请求
     TcpServer(boost::asio::io_context& io_context, int listen_port): io_context_(io_context), acceptor_(io_context, tcp::endpoint(tcp::v4(), listen_port))
     {
         startAccept();
     }
-
 };
 
 // tcp同步服务器
@@ -95,9 +103,11 @@ int main(int argc, char* argv[])
 {
     try
     {
+        // 提供I/O服务，用于接收到来的客户端连接，服务器对象将会使用
         boost::asio::io_context io_context;
         // 进行daytime协议的通信
         TcpServer server(io_context, 13);
+        // 运行io对象使之能够执行异步操作
         io_context.run();
     }
     catch (std::exception& e)
